d58445dccf27665880e80ccfc57b12452843b4cc
Geilton Xavier <geilton@vizir.com.br>
2016-04-29 15:26:58 -0300

    melhoria do codigo (refactory)

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -3,8 +3,8 @@
 
 (def total-lives 6)
 
-(defn lost [] (print "you lost!"))
-(defn win [] (print "you win!"))
+(defn lost [] (print "Você Perdeu!"))
+(defn win [] (print "Você Venceu!"))
 
 (defn missing-letters [word hits]
   (remove (fn [letter] (contains? hits(str letter))) word))
@@ -14,16 +14,24 @@
   (empty? (missing-letters word hits))
 )
 
+(defn read-leter! [] (read-line))
+
+(defn hit? [kick word] (.contains word kick))
+
 
 (defn game-play [life word hits]
-	(if ( = life 0)
-	  (lost)
-	    (if (hit-every-word? word hits)
-	    	(win)
-	    	(print "try again!")
-	    )
-	)
-)
+	(cond
+	    ( = life 0) (lost)
+	    (hit-every-word? word hits)(win)
+	    :else
+	     (let [kick (read-leter!)]
+	          (if (hit? kick word)
+	          	(do 
+	          		(println "Acertou a letra")
+	          		(game-play life word (conj hits kick)))
+		     	 (do
+		     	 	(println "Errou a letra! Perdeu vida!")
+		            (game-play (dec life) word hits))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
